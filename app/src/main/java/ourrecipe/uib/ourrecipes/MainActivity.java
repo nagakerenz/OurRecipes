ea8de93adb7d23ae2aab56e7d8406ca9ac3d89fc
a208 <didisantoso436@gmail.com>
2022-11-17 19:06:32 +0700

    Landing Page

--- app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
@@ -3,6 +3,7 @@ package ourrecipe.uib.ourrecipes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar(){
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
     }
 }
