d01d9f149caf1630e064f0dea69f2e7c8ccb1ebe
Didi Santoso <didisantoso436@gmail.com>
2022-11-29 21:12:37 +0700

    Bottom Navigation

---
+++ app/src/main/java/ourrecipe/uib/ourrecipes/ui/profile/ProfileFragment.java
@@ -0,0 +1,37 @@
+package ourrecipe.uib.ourrecipes.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import ourrecipe.uib.ourrecipes.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel settingViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textProfile;
+        settingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
