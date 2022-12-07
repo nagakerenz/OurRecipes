95fe855f33c090b561285ce088b1627c9e6f27dd
Didi Santoso <didisantoso436@gmail.com>
2022-12-07 15:29:18 +0700

    Bottom Navigation

--- app/src/main/java/ourrecipe/uib/ourrecipes/DrinkPage.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/DrinkPage.java
@@ -9,6 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DrinkPage extends AppCompatActivity {
     ImageButton back;
+    ImageButton breakfast;
+    ImageButton lunch;
+    ImageButton dinner;
+    ImageButton fiber;
+    ImageButton drink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,72 @@ public class DrinkPage extends AppCompatActivity {
                 backToHomepage();
             }
         });
+
+        breakfast = (ImageButton) findViewById(R.id.breakfast);
+        breakfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBreakfastPage();
+            }
+        });
+
+        lunch = (ImageButton) findViewById(R.id.lunch);
+        lunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLunchPage();
+            }
+        });
+        dinner = (ImageButton) findViewById(R.id.dinner);
+        dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDinnnerPage();
+            }
+        });
+        fiber = (ImageButton) findViewById(R.id.fiber);
+        fiber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFiberPage();
+            }
+        });
+        drink = (ImageButton) findViewById(R.id.drink);
+        drink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrinkPage();
+            }
+        });
     }
+
     public void backToHomepage(){
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
+        Intent back = new Intent(this, HomePage.class);
+        startActivity(back);
+    }
+
+    public void openBreakfastPage() {
+        Intent breakfast = new Intent(this, BreakfastPage.class);
+        startActivity(breakfast);
+    }
+
+    public void openLunchPage(){
+        Intent lunch = new Intent(this, LunchPage.class);
+        startActivity(lunch);
+    }
+
+    public void openDinnnerPage(){
+        Intent dinner = new Intent(this, DinnerPage.class);
+        startActivity(dinner);
+    }
+
+    public void openFiberPage(){
+        Intent fiber = new Intent(this, FiberPage.class);
+        startActivity(fiber);
+    }
+
+    public void openDrinkPage() {
+        Intent drink = new Intent(this, DrinkPage.class);
+        startActivity(drink);
     }
 }
