ece11f06dfe22af501dcbb549fabada2b37dc750
Didi Santoso <didisantoso436@gmail.com>
2022-12-07 19:51:58 +0700

    Bottom Navigation

--- app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
@@ -2,17 +2,35 @@ package ourrecipe.uib.ourrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import ourrecipe.uib.ourrecipes.ui.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeFragment();
+            }
+        });
+
         hideNavigationBar();
+    }
+
+    public void openHomeFragment() {
+        Intent homeFragment = new Intent(this, BottomNavigationBar.class);
+        startActivity(homeFragment);
     }
 
     @Override
--- app/src/main/java/ourrecipe/uib/ourrecipes/ui/home/HomeFragment.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/ui/home/HomeFragment.java
@@ -1,18 +1,34 @@
 package ourrecipe.uib.ourrecipes.ui.home;
 
+
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import kotlin.jvm.internal.Ref;
+import ourrecipe.uib.ourrecipes.BreakfastPage;
+import ourrecipe.uib.ourrecipes.DinnerPage;
+import ourrecipe.uib.ourrecipes.DrinkPage;
+import ourrecipe.uib.ourrecipes.FiberPage;
+import ourrecipe.uib.ourrecipes.LunchPage;
+import ourrecipe.uib.ourrecipes.R;
 import ourrecipe.uib.ourrecipes.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    ImageButton breakfast;
+    ImageButton lunch;
+    ImageButton dinner;
+    ImageButton fiber;
+    ImageButton drink;
 
     private FragmentHomeBinding binding;
 
@@ -20,13 +36,76 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
+        View myView = inflater.inflate(R.layout.fragment_home, container, false);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        breakfast = (ImageButton) root.findViewById(R.id.breakfast);
+        breakfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBreakfastPage();
+            }
+        });
+
+        lunch = (ImageButton) root.findViewById(R.id.lunch);
+        lunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLunchPage();
+            }
+        });
+        dinner = (ImageButton) root.findViewById(R.id.dinner);
+        dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDinnnerPage();
+            }
+        });
+        fiber = (ImageButton) root.findViewById(R.id.fiber);
+        fiber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFiberPage();
+            }
+        });
+        drink = (ImageButton) root.findViewById(R.id.drink);
+        drink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrinkPage();
+            }
+        });
         return root;
+    }
+
+    public void openBreakfastPage() {
+        Intent breakfast = new Intent(HomeFragment.this.getActivity(), BreakfastPage.class);
+        startActivity(breakfast);
+    }
+
+    public void openLunchPage(){
+        Intent lunch = new Intent(HomeFragment.this.getActivity(), LunchPage.class);
+        startActivity(lunch);
+    }
+
+    public void openDinnnerPage(){
+        Intent dinner = new Intent(HomeFragment.this.getActivity(), DinnerPage.class);
+        startActivity(dinner);
+    }
+
+    public void openFiberPage(){
+        Intent fiber = new Intent(HomeFragment.this.getActivity(), FiberPage.class);
+        startActivity(fiber);
+    }
+
+    public void openDrinkPage() {
+        Intent drink = new Intent(HomeFragment.this.getActivity(), DrinkPage.class);
+        startActivity(drink);
     }
 
     @Override
