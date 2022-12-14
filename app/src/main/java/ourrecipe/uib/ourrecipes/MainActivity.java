a6bf5e276725c1800a49824011f34a0c7e00323a
Didi Santoso <didisantoso436@gmail.com>
2022-12-14 19:12:23 +0700

    Loading Screen

--- app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
             }
-        },3000);
+        },2000);
     }
 
 
