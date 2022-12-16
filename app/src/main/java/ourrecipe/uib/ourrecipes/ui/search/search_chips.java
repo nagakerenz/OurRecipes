1cc9df2bdfd34245c6aeb940772adf0793820ba0
stephanielim <stephanielim092@gmail.com>
2022-12-16 09:01:21 +0700

    search chip

---
+++ app/src/main/java/ourrecipe/uib/ourrecipes/ui/search/search_chips.java
@@ -0,0 +1,16 @@
+package ourrecipe.uib.ourrecipes.ui.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ourrecipe.uib.ourrecipes.R;
+
+public class search_chips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_chips);
+    }
+}
