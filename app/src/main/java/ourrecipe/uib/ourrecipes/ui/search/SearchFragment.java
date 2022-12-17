e295c2797caefa631f101686bae99b5567457ece
stephanielim <stephanielim092@gmail.com>
2022-12-17 14:35:27 +0700

    sing in sign up

--- app/src/main/java/ourrecipe/uib/ourrecipes/ui/search/SearchFragment.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/ui/search/SearchFragment.java
@@ -92,7 +92,7 @@ public class SearchFragment extends Fragment {
     }
 //    dan ini
     public void openResult(){
-        Intent result = new Intent(SearchFragment.this.getActivity(), SearchResult.class);
+        Intent result = new Intent(SearchFragment.this.getActivity(), searchResult.class);
         startActivity(result);
     }
 //    sampai sini
