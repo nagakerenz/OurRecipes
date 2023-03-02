fb5b33a981d5162b074e124081226d14a6be5c96
0XY6EN <didisantoso436@gmail.com>
2023-03-02 13:10:10 +0700

    FINISHED

--- app/src/main/java/ourrecipe/uib/ourrecipes/FavoritePage.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/FavoritePage.java
@@ -5,13 +5,87 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import ourrecipe.uib.ourrecipes.ui.home.HomeFragment;
 
 public class FavoritePage extends AppCompatActivity {
+
+    ImageButton menu;
+    ImageButton menu1;
+    ImageButton menu2;
+    ImageButton menu3;
+    ImageButton menu4;
+    ImageButton menu5;
+    ImageButton menu6;
+    ImageButton menu7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_page);
 
+        menu = (ImageButton) findViewById(R.id.imageButton);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu1 = (ImageButton) findViewById(R.id.imageButton1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu2 = (ImageButton) findViewById(R.id.imageButton2);
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu3 = (ImageButton) findViewById(R.id.imageButton3);
+        menu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu4 = (ImageButton) findViewById(R.id.imageButton4);
+        menu4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu5 = (ImageButton) findViewById(R.id.imageButton5);
+        menu5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu6 = (ImageButton) findViewById(R.id.imageButton6);
+        menu6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+        menu7 = (ImageButton) findViewById(R.id.imageButton7);
+        menu7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuPage();
+            }
+        });
+    }
+
+    public void openMenuPage() {
+        Intent menu = new Intent(FavoritePage.this, MenuResult.class);
+        startActivity(menu);
     }
 }
