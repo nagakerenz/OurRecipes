65a2580cba6a2445242260698b48537acab8453e
Didi Santoso <didisantoso436@gmail.com>
2023-03-20 19:11:57 +0700

    Trying to make Username(Name) to be displayed on profile

--- app/src/main/java/ourrecipe/uib/ourrecipes/BottomNavigationBar.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/BottomNavigationBar.java
@@ -41,39 +41,39 @@ public class BottomNavigationBar extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        inputTextName = getIntent().getStringExtra("inputText");
-        navView.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new HomeFragment()).commit();
+//        inputTextName = getIntent().getStringExtra("inputText");
+//        navView.setOnNavigationItemSelectedListener(navListener);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                new HomeFragment()).commit();
 
 
         getSupportActionBar().hide();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-
-                    switch (item.getItemId()) {
-                        case R.id.nav_home:
-                            selectedFragment = new HomeFragment();
-                            break;
-                        case R.id.nav_profile:
-                            selectedFragment = new ProfileFragment();
-                            Bundle bundle = new Bundle();
-                            bundle.putString("inputText", inputText);
-                            selectedFragment.setArguments(bundle);
-                            break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+//            new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    Fragment selectedFragment = null;
+//
+//                    switch (item.getItemId()) {
+//                        case R.id.nav_home:
+//                            selectedFragment = new HomeFragment();
+//                            break;
+//                        case R.id.nav_profile:
+//                            selectedFragment = new ProfileFragment();
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("inputText", inputText);
+//                            selectedFragment.setArguments(bundle);
+//                            break;
+//                    }
+//
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                            selectedFragment).commit();
+//
+//                    return true;
+//                }
+//            };
 
 
     @Override
