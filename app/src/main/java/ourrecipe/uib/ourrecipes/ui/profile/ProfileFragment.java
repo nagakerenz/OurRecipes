695ec3e23aa42eb992ce113377ca67a2de9611fb
0XY6EN <didisantoso436@gmail.com>
2023-03-25 18:39:22 +0700

    Trying to Make User name Displayed

--- app/src/main/java/ourrecipe/uib/ourrecipes/ui/profile/ProfileFragment.java
+++ app/src/main/java/ourrecipe/uib/ourrecipes/ui/profile/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +32,8 @@ public class ProfileFragment extends Fragment {
     Button notification;
     Button account;
     TextView displayedName;
+    ImageView userPicture;
+//    FirebaseUser user;
     private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -44,22 +48,29 @@ public class ProfileFragment extends Fragment {
         notification = (Button) root.findViewById(R.id.notification);
         account = (Button) root.findViewById(R.id.account);
         displayedName = (TextView) root.findViewById(R.id.displayName);
+        userPicture = (ImageView) root.findViewById(R.id.displayPicture);
 
-        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+        
+//        user = FirebaseAuth.getInstance().getCurrentUser();
+//
+//        displayedName.setText(user.getDisplayName());
+//        Glide.with(ProfileFragment.this).load(user.getPhotoUrl()).into(userPicture);
 
-        usersRef.child(userId).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String name = snapshot.getValue(String.class);
-                displayedName.setText(name);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // Handle error
-            }
-        });
+//        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+//
+//        usersRef.child(userId).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                String name = snapshot.getValue(String.class);
+//                displayedName.setText(name);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                // Handle error
+//            }
+//        });
 
 
         favorites.setOnClickListener(new View.OnClickListener() {
